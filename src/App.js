20fd510a1ecd1a825e9be84384029b99de046ac4
gradobyk <103375580+gradobyk@users.noreply.github.com>
2022-10-13 03:57:01 +0300

    change style

--- src/App.js
+++ src/App.js
@@ -1,27 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
-function App() {
+export default function App() {
+  let weatherData = {
+    city: "New York",
+    temperature: 19,
+    date: "Tuesday 10:00",
+    description: "Cloudy",
+    imgUrl: "https://ssl.gstatic.com/onebox/weather/64/sunny.png",
+    humidity: 80,
+    wind: 10,
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <h1> not NOW</h1>
-        <h1 className='text'> final work at React week 4</h1>
-      </header>
+    <div className="Weather shadow">
+      <form className="mb-3 inside">
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Type a city you are looking for.."
+              className="form-control shadow"
+              autoComplete="off"
+            />
+          </div>
+          <div className="col-3">
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-success w-100 buttom1 shadow"
+            />
+          </div>
+        </div>
+      </form>
+      <div className="City-Time-Day">
+        <h1 className="cityName">{weatherData.city}</h1>
+        <ul>
+          <li>Last updated: {weatherData.date}</li>
+          <li>{weatherData.description}</li>
+        </ul>
+      </div>
+      <div className="row">
+        <div className="col-6">
+          <div className="temperature-parameter ">
+            <img
+              src={weatherData.imgUrl}
+              alt={weatherData.description}
+              className="float-left"
+            />
+            <strong className="float-left tempAll">
+              {weatherData.temperature}
+            </strong>
+            <span className="Temp-units">
+              <a href="/">°C</a> | <a href="/">°F</a>
+            </span>
+          </div>
+        </div>
+        <div className="col-6 other-units">
+          <ul>
+            <li>Humidity: {weatherData.humidity}%</li>
+            <li>Wind: {weatherData.wind} km/h</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
-
-export default App;
